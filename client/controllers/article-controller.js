f9d3cafe412156bf6f37f26a572879e412bc199e
Cindy Chen <cindyjan15@gmail.com>
2017-09-06 21:07:50 -0700

    update round4

--- client/controllers/article-controller.js
+++ client/controllers/article-controller.js
@@ -1,7 +1,6 @@
 var Article = require("../models/article");
 
 module.exports = {
-  // this method handles finding all articles in the db
   find: function(req, res) {
     Article.find().then(function(doc) {
       res.json(doc);
@@ -9,7 +8,6 @@ module.exports = {
       res.json(err);
     });
   },
-  // this method handles adding new articles to the db
   insert: function(req, res) {
     Article.create(req.body).then(function(doc) {
       res.json(doc);
@@ -17,7 +15,6 @@ module.exports = {
       res.json(err);
     });
   },
-  // this method handles deleting articles from the db
   delete: function(req, res) {
     Article.remove({
       _id: req.params.id
